191193312b3e56620fda4fd2ac78952fce071f1b
Camilo Andrés Concha Agudelo <cconcha@CamiloConcha.local>
2019-12-02 16:36:47 -0500

    Added components:  about page and home

---
+++ src/components/about/AboutPage.js
@@ -0,0 +1,10 @@
+import React from 'react';
+
+const AboutPage = () => (
+    <div>
+        <h2>About el concha</h2>
+        <p>This is a guy who writes some code</p>
+    </div>
+);
+
+export default AboutPage;
