243facd1592d5509c53cdbb4ee8f32a82379be75
Camilo Andrés Concha Agudelo <cconcha@CamiloConcha.local>
2019-12-05 16:04:48 -0500

    Added the courses form finishing inputs

--- src/redux/reducers/authorReducer.js
+++ src/redux/reducers/authorReducer.js
@@ -3,7 +3,6 @@ import * as types from './../actions/actionTypes';
 export default function authorReducer(state = [], action) {
     switch (action.type) {
         case types.LOAD_AUTHORS_SUCCESS:
-            debugger;
             return action.authors;
         default:
             return state;
